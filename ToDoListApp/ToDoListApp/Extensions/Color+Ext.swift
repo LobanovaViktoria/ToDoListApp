3479eec92f96be077f20d706bb40361375ad6e53
Lobanova Viktoria <va_klevtsova@mail.ru>
2024-09-02 16:46:13 +0300

    added UI for ListViewController

---
+++ ToDoListApp/ToDoListApp/Extensions/Color+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  Color+Ext.swift
+//  ToDoListApp
+//
+//  Created by Viktoria Lobanova on 02.09.2024.
+//
+
+import UIKit
+
+extension UIColor {
+   
+    // Background Colors
+    static let backgroundGray = UIColor(named: "customBackground")
+    static let backgroundWhite = UIColor(named: "customWhite")
+   
+    // Text Colors
+    static let textPrimary = UIColor(named: "customBlack") ?? UIColor.black
+    
+    // Border Colors
+    static let borderGray = UIColor(named: "borderColor")
+    static let borderBlue = UIColor(named: "customBlue")
+}
